cb0bc25f7de22704c0b6bc686d3eaef3372b9330
x-tabdeveloping <power.up1163@gmail.com>
2024-10-10 08:55:31 +0200

    Added scripts and results

---
+++ scripts/chinese_benchmark.py
@@ -0,0 +1,154 @@
+import itertools
+import json
+import os
+from pathlib import Path
+from typing import Optional
+
+import jieba
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import paired_cosine_distances
+from topic_benchmark.base import Metric
+from topic_benchmark.benchmark import run_benchmark
+from topic_benchmark.cli import load_cache
+from topic_benchmark.registries import dataset_registry, metric_registry
+from topic_benchmark.utils import get_top_k
+from topic_benchmark.metrics.wec import word_embedding_coherence
+
+from gensim.models import Word2Vec
+from gensim.corpora import Dictionary
+from gensim.models.coherencemodel import CoherenceModel
+from sklearn.feature_extraction.text import CountVectorizer
+
+from turftopic.data import TopicData
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+encoder = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+def tokenize_zh(text):
+    return jieba.lcut(text)
+
+
+with open("dat/chinese_stopwords.txt") as file:
+    stopwords = file.readlines()
+stopwords = [s.strip() for s in stopwords]
+vectorizer = CountVectorizer(stop_words=stopwords, tokenizer=tokenize_zh)
+
+
+
+def external_coherence(topics, embedding_model: SentenceTransformer):
+    arrays = []
+    for index, topic in enumerate(topics):
+        if len(topic) > 0:
+            embeddings = dict(zip(topic, embedding_model.encode(topic)))
+            w1, w2 = zip(*itertools.combinations(topic, 2))
+            e1 = np.stack([embeddings[w] for w in w1])
+            e2 = np.stack([embeddings[w] for w in w2])
+            similarities = 1 - paired_cosine_distances(e1, e2)
+            arrays.append(np.nanmean(similarities))
+    return np.nanmean(arrays)
+
+
+@metric_registry.register("zh_wec_ex")
+def load_embedding_coherence_ex() -> Metric:
+    """Multilingual external embedding coherence with a
+    multilingual sentence transformer instead of a word embedding model
+    """
+    top_k = 10
+
+    def score(data: TopicData, dataset_name: Optional[str]):
+        topics = get_top_k(data, top_k)
+        return external_coherence(topics, encoder)
+
+    return score
+
+@metric_registry.register("zh_c_npmi")
+def load_npmi() -> Metric:
+    top_k = 10
+
+    def score(data: TopicData, dataset_name: Optional[str]):
+        topics = get_top_k(data, top_k)
+        tokenizer = CountVectorizer(
+            vocabulary=data["vocab"], tokenizer=tokenize_zh, stop_words=stopwords
+        ).build_analyzer()
+        texts = [tokenizer(text) for text in data["corpus"]]
+        dictionary = Dictionary(texts)
+        cm = CoherenceModel(
+            topics=topics,
+            texts=texts,
+            dictionary=dictionary,
+            coherence="c_npmi",
+        )
+        return cm.get_coherence()
+
+    return score
+
+@metric_registry.register("zh_wec_in")
+def load_iwec() -> Metric:
+    top_k = 10
+
+    # Cache for w2v models over corpora
+    w2v_cache: dict[str, Word2Vec] = {}
+
+    def score(data: TopicData, dataset_name: Optional[str]):
+        if dataset_name not in w2v_cache:
+            tokenizer = CountVectorizer(
+                vocabulary=data["vocab"], tokenizer=tokenize_zh, stop_words=stopwords
+            ).build_analyzer()
+            texts = [tokenizer(text) for text in data["corpus"]]
+            model = Word2Vec(texts, min_count=1)
+            w2v_cache[dataset_name] = model
+        else:
+            model = w2v_cache[dataset_name]
+        topics = get_top_k(data, top_k)
+        return word_embedding_coherence(topics, model.wv)
+
+    return score
+
+CORPORA = ["chinanews", "ihuawen", "oushinet", "xinozhou", "yidali-huarenjie"]
+for corpus_name in CORPORA:
+
+    def load_corpus():
+        data = pd.read_csv(Path("dat").joinpath(f"{corpus_name}_all.csv"))
+        return list(data["text"].dropna())
+
+    dataset_registry.register(corpus_name)(load_corpus)
+
+
+def main():
+    encoder_name = "paraphrase-multilingual-MiniLM-L12-v2"
+    encoder_path_name = encoder_name.replace("/", "__")
+    out_dir = Path("results")
+    out_dir.mkdir(exist_ok=True)
+    out_path = out_dir.joinpath(f"{encoder_path_name}.jsonl")
+    out_path = f"results/{encoder_path_name}.jsonl"
+    cached_entries = load_cache(out_path)
+    print("--------------------------------------")
+    print(f"Running benchmark with {encoder_name}")
+    print("--------------------------------------")
+    entries = run_benchmark(
+        encoder=encoder,
+        vectorizer=vectorizer,
+        models=[
+            "BERTopic",
+            "NMF",
+            "LDA",
+            "Top2Vec",
+            "KeyNMF",
+            "S³",
+            "CombinedTM",
+            "ZeroShotTM",
+        ],
+        datasets=CORPORA,
+        metrics=["diversity", "zh_c_npmi", "zh_wec_in", "zh_wec_ex"],
+        seeds=(42, ),
+        prev_entries=cached_entries,
+    )
+    for entry in entries:
+        with open(out_path, "a") as out_file:
+            out_file.write(json.dumps(entry) + "\n")
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/performance_table.py
@@ -0,0 +1,99 @@
+import json
+from pathlib import Path
+from typing import Optional
+
+import pandas as pd
+
+
+def safe_index(elements: list, elem) -> Optional[int]:
+    try:
+        return elements.index(elem)
+    except ValueError:
+        return None
+
+
+records = []
+with Path(
+    "results/paraphrase-multilingual-MiniLM-L12-v2.jsonl"
+).open() as in_file:
+    for line in in_file:
+        line = line.strip()
+        if line:
+            entry = json.loads(line)
+            res = entry.pop("results")
+            entry = {**entry, **res}
+            records.append(entry)
+
+data = pd.DataFrame.from_records(records)
+
+METRICS = ["zh_c_npmi", "diversity", "zh_wec_in", "zh_wec_ex"]
+FORMATTED_METRICS = ["$C_{\\text{NPMI}}$", "$d$", "$C_{\\text{in}}$", "$C_{\\text{ex}}$"]
+MODELS = [
+    "KeyNMF",
+    "S³",
+    "Top2Vec",
+    "BERTopic",
+    "CombinedTM",
+    "ZeroShotTM",
+    "NMF",
+    "LDA",
+]
+DATASETS = [
+    "chinanews",
+    "ihuawen",
+    "oushinet",
+    "xinozhou",
+    "yidali-huarinje",
+]
+summary = data.groupby(["dataset", "model"])[METRICS].mean().reset_index()
+lines = []
+n_datasets = len(DATASETS)
+top_table_layout = "c"*(n_datasets + 1)
+lines.extend([
+    f"\\begin{{tabular}}{{{top_table_layout}}}",
+    "\\toprule",
+    "&" + " & ".join([f"\\textbf{{{dataset}}}" for dataset in DATASETS]) + "\\\\",
+    "\n",
+])
+lines.extend([
+    "\\begin{tabular}{l}",
+    "\\textbf{Model} \\\\",
+    *[f"\\textbf{{{model}}} \\\\" for model in MODELS],
+    "\\end{tabular} &"
+])
+for dataset in DATASETS:
+    res = data[data["dataset"] == "dataset"]
+    subtable_layout = "c" * len(METRICS)
+    lines.extend([
+        f"\\begin{{tabular}}{{{subtable_layout}}}",
+        " & ".join(FORMATTED_METRICS) + "\\\\",
+        "\\midrule",
+    ])
+    res = res[res["model"].isin(MODELS)]
+    res = res.set_index("model")
+    best_models = {
+        metric: list(res[metric].nlargest(2).index) for metric in METRICS
+    }
+    for model in MODELS:
+        if model not in res.index:
+            continue
+        entry = res.loc[model]
+        formatted_metrics = []
+        for metric in METRICS:
+            metric_result = entry[metric]
+            metric_formatted = f"{metric_result:.2f}"
+            model_rank = safe_index(best_models[metric], model)
+            if model_rank == 0:
+                metric_formatted = f"\\textbf{{{metric_formatted}}}"
+            if model_rank == 1:
+                metric_formatted = f"\\underline{{{metric_formatted}}}"
+            formatted_metrics.append(metric_formatted)
+        formatted_model = model
+        lines.append(
+            f"{formatted_model} &" + " & ".join(formatted_metrics) + "\\\\"
+        )
+    lines.extend([
+        "\\end{tabular} &",
+        "\n",
+    ])
+print("\n".join(lines))
