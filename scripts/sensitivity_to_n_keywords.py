bf0b76397aea8201b0632b599f45577bf3564c58
x-tabdeveloping <power.up1163@gmail.com>
2024-10-13 12:18:10 +0200

    Modified keyword sensitivity script to save to JSONL

--- scripts/sensitivity_to_n_keywords.py
+++ scripts/sensitivity_to_n_keywords.py
@@ -1,7 +1,6 @@
 import json
 from pathlib import Path
 
-import pandas as pd
 from chinese_benchmark import dataset_registry, metric_registry
 from tqdm import trange
 from turftopic import KeyNMF
@@ -21,31 +20,38 @@ def load_keywords(file_path: Path | str) -> list[dict[str, float]]:
 CORPORA = ["chinanews", "ihuawen", "oushinet", "xinozhou", "yidali-huarenjie"]
 METRICS = ["diversity", "zh_c_npmi", "zh_wec_in", "zh_wec_ex"]
 
-in_dir = Path("dat/keywords")
+def stream_entries():
+    in_dir = Path("dat/keywords")
+    for dataset_name in CORPORA:
+        print("Evaluating on ", dataset_name)
+        keyword_path = in_dir.joinpath(f"{dataset_name}_all_gen_keywords.jsonl")
+        keywords = load_keywords(keyword_path)
+        corpus = dataset_registry.get(dataset_name)()
+        for top_n in trange(5, 105, 5, desc="Evaluating on different Top N"):
+            model = KeyNMF(10, top_n=top_n)
+            doc_topic_matrix = model.fit(corpus, keywords=keywords)
+            topic_data = dict(
+                corpus=corpus,
+                vocab=model.get_vocab(),
+                document_topic_matrix=doc_topic_matrix,
+                topic_term_matrix=model.components_,
+                topic_names=model.topic_names,
+            )
+            res = {"top_n": top_n, "dataset_name": dataset_name}
+            for metric_name, metric_loader in metric_registry.get_all().items():
+                if metric_name in METRICS:
+                    metric = metric_loader()
+                    score = metric(topic_data, dataset_name)
+                    res[metric_name] = float(score)
+            yield res
 
-entries = []
-for dataset_name in CORPORA:
-    print("Evaluating on ", dataset_name)
-    keyword_path = in_dir.joinpath(f"{dataset_name}_all_gen_keywords.jsonl")
-    keywords = load_keywords(keyword_path)
-    corpus = dataset_registry.get(dataset_name)()
-    for top_n in trange(5, 105, 5, desc="Evaluating on different Top N"):
-        model = KeyNMF(10, top_n=top_n)
-        doc_topic_matrix = model.fit(corpus, keywords=keywords)
-        topic_data = dict(
-            corpus=corpus,
-            vocab=model.get_vocab(),
-            document_topic_matrix=doc_topic_matrix,
-            topic_term_matrix=model.components_,
-            topic_names=model.topic_names,
-        )
-        res = {"top_n": top_n, "dataset_name": dataset_name}
-        for metric_name, metric_loader in metric_registry.get_all().items():
-            if metric_name in METRICS:
-                metric = metric_loader()
-                score = metric(topic_data, dataset_name)
-                res[metric_name] = float(score)
-        entries.append(res)
+def main():
+    out_path = Path("dat/keyword_sensitivity.jsonl")
+    entries = stream_entries()
+    for entry in entries:
+        with open(out_path, "a") as out_file:
+            out_file.write(json.dumps(entry) + "\n")
+    print("DONE")
 
-df = pd.DataFrame.from_records(entries)
-df.to_csv("dat/keyword_sensitivity.csv")
+if __name__ == "__main__":
+    main()
